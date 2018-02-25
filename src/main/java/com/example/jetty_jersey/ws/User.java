5878b75077bf0fd5439d92804456db1ea1f50323
GUIGHIL benjamin <guighil@informatique.univ-paris-diderot.fr>
2018-02-25 15:20:21 +0100

    Ajout des class objet
    Début de bouchon User.java

---
+++ src/main/java/com/example/jetty_jersey/ws/User.java
@@ -0,0 +1,41 @@
+package com.example.jetty_jersey.ws;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+interface UserDAO {
+	static class UserClass {
+		public String login;
+		public String pass;
+	}
+	
+	UserClass getUser();
+	void retrieveUser(UserClass instance);
+}
+
+@Path("/index")
+public class User implements UserDAO {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/login")
+	public UserClass getUser() {
+		UserClass instance = new UserClass();
+		instance.login = "Benjamin";
+		instance.pass = "Mudamuda";
+
+		return instance;
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/login")
+	public void retrieveUser(UserClass instance) {
+		System.out.println(instance.login);
+	}
+
+}
