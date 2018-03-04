ebaa9b7c46fc4e2e875482dce7a387260f940121
Thomas Guignard <thomas.guignard@etu.univ-paris-diderot.fr>
2018-03-04 23:10:26 +0100

    add Location.java

--- src/main/java/com/example/jetty_jersey/ws/Location.java
+++ src/main/java/com/example/jetty_jersey/ws/Location.java
@@ -1,5 +1,52 @@
 package com.example.jetty_jersey.ws;
 
-public class Location {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+interface LocationDAO {
+	LocationClass getLocation();
+	void retrieveLocation(LocationClass instance);
+}
+
+class LocationClass {
+	public int coordX;
+	public int coordY;
+	public String name;
+	
+	public LocationClass(int coordX, int coordY) {
+		this.coordX = coordX;
+		this.coordY = coordY;
+		this.name = "Nouvel endroit";
+	}
+	
+	public LocationClass(int coordX, int coordY, String name) {
+		this.coordX = coordX;
+		this.coordY = coordY;
+		this.name = name;
+	}
+}
+
+@Path("/index")
+public class Location implements LocationDAO {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/location")
+	public LocationClass getLocation() {
+		
+		LocationClass instance = new LocationClass(50, 100, "Cafe de la Gare");
+		return instance;
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/location")
+	public void retrieveLocation(LocationClass instance) {
+		System.out.println(instance.name);
+	}
 
 }
