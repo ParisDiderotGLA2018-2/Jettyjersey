838ea14caff3673f55949de976acb2e7fa527a0c
GUIGHIL benjamin <guighil@informatique.univ-paris-diderot.fr>
2018-03-15 20:46:02 +0100

    Modification de User et Map

--- src/main/java/com/example/jetty_jersey/ws/Map.java
+++ src/main/java/com/example/jetty_jersey/ws/Map.java
@@ -23,7 +23,7 @@ class MapClass {
 	public Visibility visibilite;
 	public String name;
 	public UserClass user;
-	
+
 	public MapClass(Visibility visibilite, String name, UserClass user) {
 		this.visibilite = visibilite;
 		this.name = name;
@@ -33,15 +33,15 @@ class MapClass {
 
 class ListMapClass {
 	public ArrayList<MapClass> list;
-	
+
 	public ListMapClass() {
 		this.list = new ArrayList<MapClass>();
 	}
-	
+
 	public ListMapClass(ArrayList<MapClass> list) {
 		this.list = list;
 	}
-	
+
 	public void addMap(MapClass map) {
 		this.list.add(map);
 	}
@@ -49,65 +49,65 @@ class ListMapClass {
 
 @Path("/index")
 public class Map implements MapDAO {
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/myMaps")
+	@Path("/myMap")
 	public MapClass getMyMap() {
-		
+
 		MapClass instance = new MapClass(Visibility.PRIVATE, "Cafes", new UserClass("Benjamin"));
 
 		return instance;
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/frdMaps")
+	@Path("/frdMap")
 	public MapClass getFrdMap() {
-		
+
 		MapClass instance = new MapClass(Visibility.FRIENDS, "Cafes", new UserClass("Baptiste"));
 
 		return instance;
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/pubMaps")
+	@Path("/pubMap")
 	public MapClass getPubMap() {
-		
+
 		MapClass instance = new MapClass(Visibility.PUBLIC, "Cafes", new UserClass("Baptiste"));
 
 		return instance;
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/myMaps")
 	public ListMapClass getMyMaps() {
-		
+
 		UserClass me = new UserClass("Benjamin");
 		MapClass map1 = new MapClass(Visibility.PUBLIC, "Cafes", me);
 		MapClass map2 = new MapClass(Visibility.PRIVATE, "Vacances", me);
-		
+
 		ListMapClass instance = new ListMapClass();
 		instance.addMap(map1);
 		instance.addMap(map2);
 
 		return instance;
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/pubMaps")
 	public ListMapClass getPubMaps() {
-		
+
 		UserClass user1 = new UserClass("Baptiste");
 		UserClass user2 = new UserClass("Jude");
 		UserClass user3 = new UserClass("Mary");
 		MapClass map1 = new MapClass(Visibility.PUBLIC, "Cafes", user1);
 		MapClass map2 = new MapClass(Visibility.PUBLIC, "Places to go with friends", user2);
 		MapClass map3 = new MapClass(Visibility.PUBLIC, "Parties", user3);
-		
+
 		ListMapClass instance = new ListMapClass();
 		instance.addMap(map1);
 		instance.addMap(map2);
@@ -115,19 +115,19 @@ public class Map implements MapDAO {
 
 		return instance;
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/frdMaps")
 	public ListMapClass getFrdMaps() {
-		
+
 		UserClass user1 = new UserClass("Baptiste");
 		UserClass user2 = new UserClass("Jude");
 		UserClass user3 = new UserClass("Mary");
 		MapClass map1 = new MapClass(Visibility.FRIENDS, "Cafes", user1);
 		MapClass map2 = new MapClass(Visibility.PUBLIC, "Places to go with friends", user2);
 		MapClass map3 = new MapClass(Visibility.FRIENDS, "Parties", user3);
-		
+
 		ListMapClass instance = new ListMapClass();
 		instance.addMap(map1);
 		instance.addMap(map2);
--- src/main/java/com/example/jetty_jersey/ws/User.java
+++ src/main/java/com/example/jetty_jersey/ws/User.java
@@ -5,22 +5,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 interface UserDAO {
-	UserClass getUser();
+	UserClass getUser(@QueryParam("log") String log, @QueryParam("pass") String pass);
 	void retrieveUser(UserClass instance);
 }
 
 class UserClass {
 	public String login;
 	public String password;
-	
+
 	public UserClass(String login) {
 		this.login = login;
 		this.password = "password";
 	}
-	
+
 	public UserClass(String login, String password) {
 		this.login = login;
 		this.password = password;
@@ -29,12 +30,11 @@ class UserClass {
 
 @Path("/index")
 public class User implements UserDAO {
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/login")
-	public UserClass getUser() {
-		
+	public UserClass getUser(@QueryParam("log") String log, @QueryParam("pass") String pass) {
 		UserClass instance = new UserClass("Benjamin", "Mudamuda");
 		return instance;
 	}
